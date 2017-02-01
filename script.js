5c37177c6f66e579b261126e755a862f6fd99cab
Lars Holmberg <K3LARRA@HOTMAIL.COM>
2017-02-01 18:27:33 +0100

    Initial

---
+++ script.js
@@ -0,0 +1,11 @@
+//Wait till all content is loaded, could be external fonts scripts from other servers etc....
+if (document.readyState != 'loading'){
+  onDocumentReady();
+} else {
+  document.addEventListener('DOMContentLoaded', onDocumentReady);
+}
+
+// Page is loaded! Now event can be wired-up
+function onDocumentReady() {
+  console.log('Document ready.');
+}
