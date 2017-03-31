6455d9f8d69a482f7b240b0b002197292f9fa193
Lars Holmberg <K3LARRA@HOTMAIL.COM>
2017-03-31 20:46:35 +0200

    Added font awsomw AND NAV ARROWS

--- _posts/2017-03-01-chapter1-assignment1.md
+++ _posts/2017-03-01-chapter1-assignment1.md
@@ -6,3 +6,5 @@ permalink: /chapter101/assignment1/
 categories: "Kapitel1"
 ---
 I denna uppgift vill vi visa att webbsidor bara är vanliga filer. Det som skiljer dem från andra filer är dels filändelsen och att de är skrivna i html-format. När du använder till exempel Word och docx filändelsen så döljs den uppmärkning som gör att till exempel rubriker visas som de gör. Enkla editorer som de som nämns i uppgiften visar exakt vad som står i filen och döljer ingen uppmärkning.
+
+<i class="fa fa-camera-retro"></i>fa-camera-retro
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ function onDocumentReady() {
 }
 
 function navigation(e){
-  console.log(e.target);
+  //There can only be one active remove all active
   var navElements = document.getElementsByClassName('nav-element');
   for (var i = 0; i < navElements.length; ++i) {
     var item = navElements[i];
@@ -18,18 +18,20 @@ function navigation(e){
   }
   var navCategories = document.getElementsByClassName('nav-category');
   for (var i = 0; i < navCategories.length; ++i) {
-    var item = navCategories[i]; 
+    var item = navCategories[i];
     item.classList.remove("active");
   }
   e.target.classList.add("active");
   var element = e.target.parentNode;
-  //console.log(element.nextElementSibling.nodeName);
+  //IF next element is UL then it is a nav-category we clicked so lets expand
   if(element.nextElementSibling!==null&&element.nextElementSibling.nodeName=="UL"){  //Check if it is sub
       console.log("JEpp");
     if (element.nextElementSibling.style.display===""||element.nextElementSibling.style.display==="none"){
       element.nextElementSibling.style.display="block";
+      e.target.classList.add("expanded");
     }else{
       element.nextElementSibling.style.display="none";
+      e.target.classList.remove("expanded");
     }
   }
 }
